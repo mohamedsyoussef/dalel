3b2d0842753a7fd6de4973a4bbbbae995d8c00db
mohamedsyoussef <72705926+mohamedsyoussef@users.noreply.github.com>
2024-05-04 01:23:45 +0300

    Add initial app assets configuration
    Added initial configuration for app assets including images for onboarding
    screens and a logo. Also included classes for app colors, constants, functions,
    and strings. Updated pubspec.yaml to specify assets path and output file for
    assets configuration.

---
+++ lib/core/utils/app_assets.dart
@@ -0,0 +1,20 @@
+class Assets {
+  Assets._();
+  
+  /// Assets for assetsImagesDalel
+  /// assets/images/Dalel.png
+  static const String assetsImagesDalel = "assets/images/Dalel.png";
+
+  /// Assets for assetsImagesOnboarding1
+  /// assets/images/onboarding1.png
+  static const String assetsImagesOnboarding1 = "assets/images/onboarding1.png";
+
+  /// Assets for assetsImagesOnboarding2
+  /// assets/images/onboarding2.png
+  static const String assetsImagesOnboarding2 = "assets/images/onboarding2.png";
+
+  /// Assets for assetsImagesOnboarding3
+  /// assets/images/onboarding3.png
+  static const String assetsImagesOnboarding3 = "assets/images/onboarding3.png";
+}
+
