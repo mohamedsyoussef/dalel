8fcdb1ef1f1d11c5976bbf8704a2d9e7097cdc5c
mohamedsyoussef <72705926+mohamedsyoussef@users.noreply.github.com>
2024-05-08 14:00:48 +0300

    Add firebase_auth plugin and check for authentication state changes
    - Added the firebase_auth plugin to the project dependencies.
    - Implemented a function to check for authentication state changes using Firebase Auth.
    - Updated imports and references to use the new DalelApp class.
    - Updated the macOS and Windows plugin registrants to include firebase_auth.
    - Updated pubspec.lock with the new firebase_auth version.

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
-import 'package:dalel/app/dalel.dart';
+import 'package:dalel/app/dalel_app.dart';
 import 'package:dalel/core/database/cache/cache_helper.dart';
+import 'package:dalel/core/functions/check_state_changes.dart';
 import 'package:dalel/core/services/service_locator.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
@@ -12,5 +13,6 @@ void main() async {
     options: DefaultFirebaseOptions.currentPlatform,
   );
   await getIt<CacheHelper>().init();
+  checkAuthStateChanges();
   runApp(const Dalel());
 }
