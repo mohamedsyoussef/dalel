b75199d76f764b813e29d75d1ab0e54c56b613df
mohamedsyoussef <72705926+mohamedsyoussef@users.noreply.github.com>
2024-05-08 13:36:18 +0300

    Add FlutterFire configuration and Firebase options
    Add FlutterFire configuration for Google services in the Android
    build.gradle and settings.gradle files. Also, include Firebase options
    for different platforms in the newly added firebase_options.dart file.

--- lib/main.dart
+++ lib/main.dart
@@ -2,10 +2,15 @@ import 'package:dalel/app/dalel.dart';
 import 'package:dalel/core/database/cache/cache_helper.dart';
 import 'package:dalel/core/services/service_locator.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   setupServiceLocator();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   await getIt<CacheHelper>().init();
   runApp(const Dalel());
 }
